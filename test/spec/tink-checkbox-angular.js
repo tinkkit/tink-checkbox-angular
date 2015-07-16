c1e2e49eff2eaed716c0fab3d8892e664db92fd2
Vincent Bouillart <vincentbouillart@live.be>
2015-07-16 13:44:47 +0200

    add test

--- test/spec/tink-checkbox-angular.js
+++ test/spec/tink-checkbox-angular.js
@@ -2,9 +2,9 @@
 describe('tink-checkbox-angular', function() {
 
   var bodyEl = $('body'), sandboxEl;
-  var $compile, $templateCache, scope;
+  var $compile, $templateCache, scope,obj;
 
-  // beforeEach(module('tink'));
+  beforeEach(module('tink.checkbox'));
 
   beforeEach(inject(function (_$rootScope_, _$compile_, _$templateCache_) {
     scope = _$rootScope_.$new();
@@ -12,6 +12,92 @@ describe('tink-checkbox-angular', function() {
     $templateCache = _$templateCache_;
     bodyEl.html('');
     sandboxEl = $('<div>').attr('id', 'sandbox').appendTo(bodyEl);
+
+    obj = {dataY:[]};
+      obj.dataX = [
+    {
+                  id:'33',
+                  name:'Lubbeekstraat',
+                  
+                  children:[]
+                  },
+
+                      {
+                  id:'343',
+                  name:'Lubbeekstraat',
+                  },
+        {
+          id:'1',
+          name:'Belgie',
+          
+          children:[
+            {
+              id:'2',
+              name:'Antwerpen',
+              
+            },
+            {
+              id:'3',
+              name:'Vlaams-brabant',
+              
+              children:[
+                {
+                id:'4',
+                name:'Leuven',
+                
+                },
+                {
+                id:'5',
+                name:'Heverlee',
+                
+                  children:[
+                  {
+                  id:'6',
+                  name:'Doleegstraat',
+                  },
+                  {
+                  id:'7',
+                  name:'Bergstraat',
+                  children:[
+                  {
+                  id:'8',
+                  name:'Doleegstraat',
+                  },
+                  {
+                  id:'9',
+                  name:'Bergstraat',
+                  children:[
+                  {
+                  id:'10',
+                  name:'Doleegstraat',
+                  },
+                  {
+                  id:'11',
+                  name:'Bergstraat',
+                  },
+                  {
+                  id:'12',
+                  name:'Lubbeekstraat',
+                  },
+                ]
+                  },
+                  {
+                  id:'13',
+                  name:'Lubbeekstraat',
+                  },
+                ]
+                  },
+                  {
+                  id:'14',
+                  name:'Lubbeekstraat',
+                  },
+                ]
+                },
+              ]
+            }
+          ]
+        }
+      ];
   }));
 
   afterEach(function() {
@@ -31,13 +117,38 @@ describe('tink-checkbox-angular', function() {
   var templates = {
     'default': {
       scope: {},
-      element: ''
+      element: '<div tink-checkbox-list="masterc" data-checked="dataY" ng-model="dataX" class="form-group"></div>'
     }
   };
 
+ 
 
   describe('default', function() {
     it('should run this basic setup',function(){
+      var element = compileDirective('default',obj);
+      sandboxEl.find('input[id=id3]').click();
+      scope.$digest();
+      expect(scope.dataY).toEqual(['3','4','5','6','7','8','9','10','11','12','13','14'])
+      console.log(sandboxEl.find('input[id=id3]')[0])
+    });
+
+    it('should unslect a box',function(){
+      var element = compileDirective('default',obj);
+      sandboxEl.find('input[id=id3]').click();
+      scope.$digest();
+      sandboxEl.find('input[id=id8]').click();
+      scope.$digest();
+      expect(scope.dataY).toEqual(['4','6','9','10','11','12','13','14'])
+    });
+
+    it('should have indeterminate class',function(){
+      var element = compileDirective('default',obj);
+      sandboxEl.find('input[id=id3]').click();
+      scope.$digest();
+      sandboxEl.find('input[id=id8]').click();
+      scope.$digest();
+
+      expect(sandboxEl.find('input[id=id5]').hasClass('indeterminate')).toEqual(true);
     });
   });
 
